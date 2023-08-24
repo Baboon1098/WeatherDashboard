bdeb05f9f0fd4a44537ada1effe2a7f607a2b0c6
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-08-23 21:34:31 -0500

    Final

---
+++ Homepage.js
@@ -0,0 +1,78 @@
+const apiKey = 'f5bf95abb0464b637bc44c46c459808d';
+const getForecastButton = document.getElementById('getForecast');
+const cityInput = document.getElementById('city');
+const currentWeather = document.getElementById('current-weather');
+const forecastInfo = document.getElementById('forecast-info');
+
+getForecastButton.addEventListener('click', () => {
+  const cityName = cityInput.value;
+  if (cityName) {
+    getForecast(cityName);
+  }
+});
+
+function getForecast(city) {
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+
+  fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+      const currentTemperature = data.main.temp;
+      const currentWeatherDescription = data.weather[0].description;
+      const currentWeatherIcon = data.weather[0].icon;
+      const currentDate = new Date().toLocaleDateString();
+
+      currentWeather.innerHTML = `
+        <p>Current City: ${city} - ${currentDate}</p>
+        <p>Current Temperature: ${currentTemperature}°F</p>
+        <p>Current Weather: ${currentWeatherDescription}</p>
+        <img class="weather-icon" src="http://openweathermap.org/img/w/${currentWeatherIcon}.png" alt="${currentWeatherDescription}">
+      `;
+
+      return fetch(forecastUrl);
+    })
+    .then(response => response.json())
+    .then(data => {
+      const forecasts = data.list;
+      let forecastHtml = '';
+
+      const dailyForecasts = {};
+
+      forecasts.forEach(forecast => {
+        const date = new Date(forecast.dt * 1000);
+        const day = date.toLocaleDateString();
+
+        if (!dailyForecasts[day]) {
+          dailyForecasts[day] = {
+            temperature: forecast.main.temp,
+            weatherDescription: forecast.weather[0].description,
+            weatherIcon: forecast.weather[0].icon
+          };
+        }
+      });
+
+      forecastHtml += '<div class="forecast-box">';
+      for (const day in dailyForecasts) {
+        const { temperature, weatherDescription, weatherIcon } = dailyForecasts[day];
+
+        forecastHtml += `
+          <div class="forecast-item">
+            <p>Date: ${day}</p>
+            <p>Temperature: ${temperature}°F</p>
+            <p>Description: ${weatherDescription}</p>
+            <img class="weather-icon" src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherDescription}">
+          </div>
+        `;
+      }
+      forecastHtml += '</div>';
+
+      forecastInfo.innerHTML = forecastHtml;
+    })
+    .catch(error => {
+      console.error('Error fetching forecast data:', error);
+      currentWeather.innerHTML = '<p>Failed to fetch weather data. Please try again later.</p>';
+      forecastInfo.innerHTML = '';
+    });
+}
+  
